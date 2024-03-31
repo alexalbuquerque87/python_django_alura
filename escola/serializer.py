826f9099bd0eef42c25ef3b2f6f9a52faf506df7
Alex <alexalbuquerqueti@gmail.com>
2024-03-31 16:11:11 -0300

    Initial commit

---
+++ escola/serializer.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from escola.models import Aluno, Curso, Matricula
+
+class AlunoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento']
+
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = '__all__' # __all__ é igual a criar uma lista com todos os campos semelhante ao fields do aluno
+
+class MatriculaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
+        exclude = [] # exclude trás todos os campos, exceto os especificados. Eg: exclude = ['id'], trás todos os campos exceto id
+
+class ListaMatriculasAlunoSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(source='curso.descricao')
+    periodo = serializers.SerializerMethodField()
+    class Meta:
+        model = Matricula
+        fields = ['curso', 'periodo']
+    def get_periodo(self, obj):
+        return obj.get_periodo_display()
+
+class ListaAlunosMatriculadosSerializer(serializers.ModelSerializer):
+    aluno_nome = serializers.ReadOnlyField(source='aluno.nome')
+    class Meta:
+        model = Matricula
+        fields = ['aluno_nome']
